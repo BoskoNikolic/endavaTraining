fa08508472299cd6682bd1c0b734d59d6ed90411
Jovan P <janikfin@gmail.com>
2019-10-15 23:53:05 +0200

    First commit, T8 - Register New Account (Wrong Registration Code).

---
+++ endavaTraining/src/main/java/com/endavatraining/pages/RegisterNewAccountPage.java
@@ -0,0 +1,41 @@
+package com.endavatraining.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RegisterNewAccountPage extends BasePage {
+
+    private By userNameField = By.xpath("//div[@id='registeruserinfo' and contains(@style,'display: block')]/div[1]");
+    private By registrationCodeField = By.id("registrationCode");
+    private By registrationCodeErrorMessage = By.xpath("//div[@id='regiCodeMessage'][contains(.,\"Bla bla incorect registration code format!\")]");
+
+
+    public RegisterNewAccountPage(WebDriver driver) {
+        super(driver);
+    }
+
+    /**
+     *
+     * This method enters text in Registration Code text field and checks is mentioned element visible
+     *
+     * @author Jovan.Penic
+     * @param enterRandomRegistrationCode
+     * @return
+     */
+    public boolean isUserNameFieldPresentAfterSpecialCharacterEntryInRegistrationCodeField(String enterRandomRegistrationCode) {
+        driver.findElement(registrationCodeField).sendKeys(enterRandomRegistrationCode);
+        return isElementPresent(userNameField);
+    }
+
+    /**
+     *
+     * This method checks if registration code error is visible
+     *
+     * @author Jovan.Penic
+     */
+    public boolean isErrorMessagePresentAfterFalseEntryInRegistrationCodeField() {
+        return isElementPresent(registrationCodeErrorMessage);
+    }
+
+
+}
---
+++ endavaTraining/src/test/java/com/endavatraining/TestRegisterNewAccountPage.java
@@ -0,0 +1,62 @@
+package com.endavatraining;
+
+import com.endavatraining.pages.LoginPage;
+import com.endavatraining.pages.RegisterNewAccountPage;
+import com.endavatraining.util.Utils;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestRegisterNewAccountPage {
+
+    private RegisterNewAccountPage registerNewAccountPage;
+    private LoginPage loginPage;
+    private static String randomRegistrationCodeWithSpecCharacters = "+++123";
+    private static String randomRegistrationCodeWithLettersAndNumbers = "abc123";
+
+    @BeforeTest
+    @Parameters({"browser"})
+    public void setUp(String browser) {
+        loginPage = Utils.setUpWebBrowser(browser);
+    }
+
+    /**
+     *
+     * Test validates that username field is NOT visible after entering special characters in Registration Code text field.
+     * Then test validates if registration code error IS visible.
+     *
+     *  @author Jovan.Penic
+     */
+    @Test (priority = 0)
+    public void test1IsUserFieldVisibleRegiCodeSpecialCharacters() {
+        registerNewAccountPage = loginPage.openCreateAccount();
+        Assert.assertFalse(registerNewAccountPage.isUserNameFieldPresentAfterSpecialCharacterEntryInRegistrationCodeField(randomRegistrationCodeWithSpecCharacters), "Username field IS present after special character entry in registration code field");
+        Assert.assertTrue(registerNewAccountPage.isErrorMessagePresentAfterFalseEntryInRegistrationCodeField(), "Error message does NOT appear after special character entry in registration code field");
+    }
+
+    /**
+     *
+     * Test validates that username field IS visible after entering special characters in Registration Code text field of create.
+     * Then test validates if registration code error is NOT visible.
+     *
+     *  @author Jovan.Penic
+     */
+    @Test (priority = 1)
+    public void test2IsUserFieldVisibleRegiCodeLettersAndNumbers() {
+        registerNewAccountPage = loginPage.openCreateAccount();
+        Assert.assertTrue(registerNewAccountPage.isUserNameFieldPresentAfterSpecialCharacterEntryInRegistrationCodeField(randomRegistrationCodeWithLettersAndNumbers),"Username field is NOT present after random letters and numbers entry in registration code field");
+        Assert.assertFalse(registerNewAccountPage.isErrorMessagePresentAfterFalseEntryInRegistrationCodeField(), "Error message DOES appear after random letters and numbers entry in registration code field");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (loginPage != null)
+            loginPage.quit();
+        if (registerNewAccountPage != null)
+            registerNewAccountPage.quit();
+    }
+
+
+}

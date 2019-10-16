b5774b22a86391b443b523a248bfe2c141f2c89b
jovan.penic <jovan.penic@endava.com>
2019-10-16 15:58:38 +0200

    First commit, T20 - Test Image Caption

--- endavaTraining/src/test/java/com/endavatraining/TestGalleryPage.java
+++ endavaTraining/src/test/java/com/endavatraining/TestGalleryPage.java
@@ -1,4 +1,55 @@
 package com.endavatraining;
 
+import com.endavatraining.pages.GalleryPage;
+import com.endavatraining.pages.LoginPage;
+import com.endavatraining.util.Utils;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
 public class TestGalleryPage {
+
+    private GalleryPage galleryPage;
+    private LoginPage loginPage;
+    private static String usernameLogInValue = "user";
+    private static String passwordLogInValue = "password";
+    private By firstPictureInGallery  = By.xpath("//img[@onclick='openModal();currentSlide(1)']");
+
+
+    @BeforeTest
+    @Parameters({"browser"})
+    public void setUp(String browser) {
+        loginPage = Utils.setUpWebBrowser(browser);
+    }
+
+    /**
+     *
+     * Test validates that username field is NOT visible after entering special characters in Registration Code text field.!!!!!!!!!!!!!!!
+     * Then test validates if registration code error IS visible.!!!!!!!!!!!!!!!!!!!!!!!!
+     *
+     *  @author Jovan.Penic
+     */
+    @Test (priority = 0)
+    public void testAreImageCaptionsVisible() throws InterruptedException {
+        loginPage.userLogin(usernameLogInValue, passwordLogInValue);
+        galleryPage = loginPage.openGalleryPage();
+        galleryPage.driver.findElement(firstPictureInGallery).click();
+        Thread.sleep(2000);
+
+//        Assert.assertFalse(registerNewAccountPage.isUserNameFieldPresentAfterSpecialCharacterEntryInRegistrationCodeField(randomRegistrationCodeWithSpecCharacters), "Username field IS present after special character entry in registration code field. ");
+ //       Assert.assertTrue(registerNewAccountPage.isErrorMessagePresentAfterFalseEntryInRegistrationCodeField(), "Error message does NOT appear after special character entry in registration code field. ");
+    }
+
+
+    @AfterTest
+    public void tearDown() {
+        if (loginPage != null)
+            loginPage.quit();
+        if (galleryPage != null)
+            galleryPage.quit();
+    }
+
+
+
+
 }

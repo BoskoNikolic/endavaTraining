d4ffdfe053bcfd60f09218cf2af21f76fc0aaab6
jovan.penic <jovan.penic@endava.com>
2019-10-17 16:59:27 +0200

    Final commit, T8 - Register New Account (Wrong Registration Code)

--- endavaTraining/src/test/java/com/endavatraining/TestLoginPage.java
+++ endavaTraining/src/test/java/com/endavatraining/TestLoginPage.java
@@ -1,7 +1,6 @@
 package com.endavatraining;
 
 import org.apache.log4j.Logger;
-import com.endavatraining.pages.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;

ad2172c6b05a704f3939531d0acab05ce587bbfd
luka.ivancic <luka.ivancic@endava.com>
2019-10-17 12:20:21 +0200

    T4 2.0

--- endavaTraining/src/main/java/com/endavatraining/pages/HomePage.java
+++ endavaTraining/src/main/java/com/endavatraining/pages/HomePage.java
@@ -9,7 +9,6 @@ public class HomePage extends BasePage {
 
 	private By homePageWelcomeMessage = By.xpath("//div[@class='panel-title text-center'][contains(.,'Hello, and welcome to our gamers page!')]");
 	public static Logger log = Logger.getLogger(HomePage.class);
-	private By homePageWelcomeMessage = By.xpath("//div[@class='panel-title text-center'][contains(.,'Hello, and welcome to our gamers page!')]"); //<div class="panel-title text-center">Hello, and welcome to our gamers page!</div>
 	private By adminTab = By.linkText("Admin");
 
 	public HomePage(WebDriver driver) {

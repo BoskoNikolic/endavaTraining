c7c917dd25ef8cfd1c18294546e7d72412907801
todor0312 <56254611+todor0312@users.noreply.github.com>
2019-10-18 09:46:59 +0200

    Update HomePage.java

--- endavaTraining/src/main/java/com/endavatraining/pages/HomePage.java
+++ endavaTraining/src/main/java/com/endavatraining/pages/HomePage.java
@@ -1,34 +1,22 @@
 package com.endavatraining.pages;
 
-import org.apache.log4j.Logger;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
 public class HomePage extends BasePage {
 
-    private By homePageWelcomeMessage = By.xpath("//div[@class='panel-title text-center'][contains(.,'Hello, and welcome to our gamers page!')]");
-    public static Logger log = Logger.getLogger(HomePage.class);
-
-    public HomePage(WebDriver driver) {
-        super(driver);
-    }
+	private By homePageWelcomeMessage = By.xpath("//div[@class='panel-title text-center'][contains(.,'Hello, and welcome to our gamers page!')]");
 
 
-    public boolean isWelcomeTextPresent() {
-        return isElementPresent(homePageWelcomeMessage);
-    }
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+	
 
-    /*
-     *
-     * This method is used to navigate from home page to another page
-     * @author Srboljub.Todorovic
-     * @param pageTab
-     */
-
-    public void goToPage(By pageTab) {
-
-        driver.findElement(pageTab).click();
-    }
+	public boolean isWelcomeTextPresent() {
+		return isElementPresent(homePageWelcomeMessage);
+	}
 
 }

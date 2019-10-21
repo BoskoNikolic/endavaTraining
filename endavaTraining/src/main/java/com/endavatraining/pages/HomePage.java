abc390ff3e0d54ccf26040551f9aed8989723f16
luka.ivancic <luka.ivancic@endava.com>
2019-10-21 15:46:09 +0200

    T12_luka.ivancic Testing if Admins details are the same in the Users table, and on the Details page - first commit

--- endavaTraining/src/main/java/com/endavatraining/pages/HomePage.java
+++ endavaTraining/src/main/java/com/endavatraining/pages/HomePage.java
@@ -1,22 +1,32 @@
 package com.endavatraining.pages;
 
-
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 
 public class HomePage extends BasePage {
 
-	private By homePageWelcomeMessage = By.xpath("//div[@class='panel-title text-center'][contains(.,'Hello, and welcome to our gamers page!')]");
 
+	private By homePageWelcomeMessage = By.xpath("//div[@class='panel-title text-center'][contains(.,'Hello, and welcome to our gamers page!')]");
+	private By usersPage = By.linkText("Users");
+
+	public static Logger log = Logger.getLogger(HomePage.class);
 
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
-	
+
 
 	public boolean isWelcomeTextPresent() {
 		return isElementPresent(homePageWelcomeMessage);
+	}
+
+	public WebElement findUsersPage(){
+
+		return driver.findElement(this.usersPage);
+
 	}
 
 	/*
@@ -28,7 +38,5 @@ public class HomePage extends BasePage {
 	public void goToPage(By pageTab) {
 		driver.findElement(pageTab).click();
 	}
-
-
 
 }

37efcdabe90a3773ace6f2f60306e30816b8ccec
Your Name <you@example.com>
2020-03-11 14:19:07 +0100

    few cosmetic modification

--- endavaTraining/src/test/java/com/endavatraining/BaseTest.java
+++ endavaTraining/src/test/java/com/endavatraining/BaseTest.java
@@ -11,38 +11,31 @@ import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
-    protected static BasePage basePage;
-    public static Logger log = Logger.getLogger(BaseTest.class);
-    public static final String ADMIN_USERNAME = "admin";
-    public static final String ADMIN_PASSWORD = "password";
-    public static final String USER_USERNAME = "user";
-    public static final String USER_PASSWORD = "password";
+	protected static BasePage basePage;
+	public static Logger log = Logger.getLogger(BaseTest.class);
+	public static final String ADMIN_USERNAME = "admin";
+	public static final String ADMIN_PASSWORD = "password";
+	public static final String USER_USERNAME = "user";
+	public static final String USER_PASSWORD = "password";
 
-    @BeforeClass
-    @Parameters({"browser"})
-    public void setUp(String browser) {
-        basePage = Utils.setUpWebBrowser(browser);
-    }
+	@BeforeClass
+	@Parameters({ "browser" })
+	public void setUp(String browser) {
+		basePage = Utils.setUpWebBrowser(browser);
+	}
 
-    /**
-     * This method runs after every test and takes screen shot if test fails
-     *
-     * @author Jovan.Penic
-     * @param result
-     */
-    @AfterMethod
-    public void testFailScreenshot(ITestResult result){
-        if (ITestResult.FAILURE == result.getStatus()){
-            Utils.captureScreenshot(basePage.driver, result.getName());
-            log.info("Screenshot of failure of test " + result.getName() +" is taken!");
-        }
-    }
+	@AfterMethod
+	public void testFailScreenshot(ITestResult result) {
+		if (ITestResult.FAILURE == result.getStatus()) {
+			Utils.captureScreenshot(basePage.driver, result.getName());
+			log.info("Screenshot of failure of test " + result.getName() + " is taken!");
+		}
+	}
 
-    @AfterClass
-    public void tearDown() {
-        if (basePage != null)
-            basePage.quit();
-    }
-
+	@AfterClass
+	public void tearDown() {
+		if (basePage != null)
+			basePage.quit();
+	}
 
 }
--- endavaTraining/src/test/java/com/endavatraining/TestAlgorithmsPage.java
+++ endavaTraining/src/test/java/com/endavatraining/TestAlgorithmsPage.java
@@ -1,38 +1,42 @@
 package com.endavatraining;
 
-import com.endavatraining.pages.AlgorithmsPage;
-import com.endavatraining.pages.HomePage;
-import com.endavatraining.pages.LoginPage;
-import com.endavatraining.util.Utils;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.endavatraining.pages.AlgorithmsPage;
+import com.endavatraining.pages.HomePage;
+import com.endavatraining.pages.LoginPage;
+import com.endavatraining.util.Utils;
 
 public class TestAlgorithmsPage extends BaseTest {
 
-    private LoginPage loginPage;
-    private HomePage homePage;
-    private AlgorithmsPage algorithmsPage;
-    private By algorithmsPageButton = By.linkText("Algorithms");
-    public static Logger log = Logger.getLogger(TestAlgorithmsPage.class);
-    
-	private  String username = "user";
-	private  String password = "password";
-	private  String formControlValue = "8";
-	private  String searchKeyValue = "ana";
-	private  String vgnKeyValue = "FJv6Jb";
-	private  String letterValue = "e";
+	private LoginPage loginPage;
+	private HomePage homePage;
+	private AlgorithmsPage algorithmsPage;
+	private By algorithmsPageButton = By.linkText("Algorithms");
+	public static Logger log = Logger.getLogger(TestAlgorithmsPage.class);
+
+	private String username = "user";
+	private String password = "password";
+	private String formControlValue = "8";
+	private String searchKeyValue = "ana";
+	private String vgnKeyValue = "FJv6Jb";
+	private String letterValue = "e";
 	private By userNameBy = By.id("username");
 	private By passWordBy = By.id("password");
 
+	@BeforeTest
+	@Parameters({ "browser" })
+	public void setUp(String browser) {
+		loginPage = Utils.setUpWebBrowser(browser);
+	}
 
-    @BeforeTest
-    @Parameters({"browser"})
-    public void setUp(String browser) {
-        loginPage = Utils.setUpWebBrowser(browser);
-    }
 	/*
 	 * Test validates that algorithms page is opened by checking if Submit button is
 	 * visible on the page
@@ -41,69 +45,76 @@ public class TestAlgorithmsPage extends BaseTest {
 	@Test
 	public void testAlgorithmsPageIsOpened() {
 		algorithmsPage = new AlgorithmsPage(homePage.driver);
-		
-		//1. User is logged and home page is present
+
+		// 1. User is logged and home page is present
 		log.info("Test is Algorithms Page is opened");
 		homePage = loginPage.openAs(USER_USERNAME, USER_PASSWORD);
-		//2. Click to Algorithms Tab to open Algorithms Page
+		// 2. Click to Algorithms Tab to open Algorithms Page
 		algorithmsPage.openArithmeticsPage();
-		//3. Verify that Algorithms Page is presented
+		// 3. Verify that Algorithms Page is presented
 		new WebDriverWait(algorithmsPage.driver, 5)
 				.until(ExpectedConditions.visibilityOfElementLocated(algorithmsPage.getSubmitButton()));
 	}
+
 	/*
-	 * Test validates is Log In page available by clicking upperRightLogInButton 
+	 * Test validates is Log In page available by clicking upperRightLogInButton
 	 */
 	@Parameters("browser")
 	@Test
 	public void testSubmitButtonFunctionality() {
-		//1. Verify that Algorithms Page is presented
+		// 1. Verify that Algorithms Page is presented
 		testAlgorithmsPageIsOpened();
-		//2. Click to the RightUpperLoginButton
-        log.info("Clear and enter text into the formControl field" );
-        algorithmsPage.submitNewValue(formControlValue, searchKeyValue, vgnKeyValue, letterValue);
-        //3. Verify that RightUpperLoginButton open login Page
+		// 2. Click to the RightUpperLoginButton
+		log.info("Clear and enter text into the formControl field");
+		algorithmsPage.submitNewValue(formControlValue, searchKeyValue, vgnKeyValue, letterValue);
+		// 3. Verify that RightUpperLoginButton open login Page
 		new WebDriverWait(algorithmsPage.driver, 5)
 				.until(ExpectedConditions.visibilityOfElementLocated(algorithmsPage.getLoginButton()));
 	}
+
 	/*
-	 * *Test validates is random string expected 
+	 * *Test validates is random string expected
 	 */
 	@Parameters("browser")
 	@Test
 	public void testRandomString() throws Exception {
-		//1. precondition for generated prime numbers verification: enter and submit appropriate field value
+		// 1. precondition for generated prime numbers verification: enter and submit
+		// appropriate field value
 		testSubmitButtonFunctionality();
-		//2. Random string verification
+		// 2. Random string verification
 		algorithmsPage.randomStringVerification();
 	}
+
 	/*
-	 * *Test validates is Letters field expected 
+	 * *Test validates is Letters field expected
 	 */
 	@Parameters("browser")
 	@Test
 	public void testLettersFieldverification() throws Exception {
-		//1. precondition for generated prime numbers verification: enter and submit appropriate field value
+		// 1. precondition for generated prime numbers verification: enter and submit
+		// appropriate field value
 		testSubmitButtonFunctionality();
-		//2. Letters field verification
+		// 2. Letters field verification
 		algorithmsPage.randomLettersVerification();
 	}
+
 	/*
-	 * *Test validates is Digits field expected 
+	 * *Test validates is Digits field expected
 	 */
 	@Parameters("browser")
 	@Test
 	public void testDigitsFieldverification() throws Exception {
-		//1. precondition for generated prime numbers verification: enter and submit appropriate field value
+		// 1. precondition for generated prime numbers verification: enter and submit
+		// appropriate field value
 		testSubmitButtonFunctionality();
-		//2. Digit field verification
+		// 2. Digit field verification
 		algorithmsPage.randomDigitsVerification();
 	}
 
-    @AfterClass
-    public void tearDown() {
-        if (algorithmsPage != null)
-        	algorithmsPage.quit();
-    }
-	
+	@AfterClass
+	public void tearDown() {
+		if (algorithmsPage != null)
+			algorithmsPage.quit();
+	}
+
 }
